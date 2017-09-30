ce3f13687311914212ac3a21aeecab9b39649bf2
Tom Lant <lampholder@gmail.com>
2017-09-30 15:50:24 +0100

    Now with AWS cognito gubbins

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" }
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.json$/, loader: "json" }
         ]
     }
 };
