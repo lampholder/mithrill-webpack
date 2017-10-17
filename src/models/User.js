c289006ab97702354564749bf12cb82fb4b79ff1
Tom Lant <lampholder@gmail.com>
2017-10-17 15:05:09 +0100

    login function|

--- src/models/User.js
+++ src/models/User.js
@@ -3,24 +3,48 @@ var m = require('mithril');
 
 var Cognito = require('amazon-cognito-identity-js');
 
+var COMPANY_NAME = 'acme';
+
 var poolData = {
-    UserPoolId: 'eu-west-1_L52bqGTik',
-    ClientId: '6d0rd8npg1dq6ks046e5lt8vp5'
+    UserPoolId: 'eu-west-1_58XkXQ32W',
+    ClientId: '5l9m7qjfovoavah9abkgbn1d29'
 };
 var userPool = new Cognito.CognitoUserPool(poolData);
 
 var User = {};
 User.register = function(user) {
-    var attributeList = [];
-
     var cognitoUser;
-    userPool.signUp(user.email, user.password, attributeList, null, function(err, result){
+    var emailData = {
+        Name: 'email',
+        Value: user.email
+    };
+
+    var emailAttribute = Cognito.CognitoUserAttribute(emailData);
+
+    userPool.signUp(COMPANY_NAME + '-' + user.email, user.password, [emailAttribute], null, function(err, result){
         if (err) {
             alert(err);
             return;
         }
         cognitoUser = result.user;
         console.log('user name is ' + cognitoUser.getUsername());
+    });
+};
+
+User.login = function(user) {
+    var userNmae = COMPANY_NAME + '-' + user.email;
+
+    var cognitoUser = new Cognito.CognitoUser({Username: userName,
+                                               Pool: userPool});
+    var auth = new Cognito.AuthenticationDetails({Username: userName,
+                                                  Password: user.password});
+    cognitoUser.authenticateUser(auth, {
+        onSuccess: function(result) {
+            alert('yep, you\'re a good\'un');
+        },
+        onFailure: function(err) {
+            alert(err);
+        }
     });
 };
 
