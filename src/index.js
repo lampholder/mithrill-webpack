8b36dfe613da0623f3400c5a93f279782414559e
Tom Lant <lampholder@gmail.com>
2017-10-17 15:04:49 +0100

    mount multiple pages

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ var m = require('mithril');
 
 require('./css/style.css');
 
-var Registration = require("./views/Registration");
-m.mount(document.body, Registration);
+var Registration = require('./views/Registration');
+var Login = require('./views/Login');
+
+m.route(document.body, '/login', {
+    '/login': Login,
+    '/register':  Registration
+});
